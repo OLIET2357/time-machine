3db070f17a2de3f61acb6bacde5c7e7838dcfcff
OLIET2357 <oliet2357@gmail.com>
2022-01-11 14:30:13 +0900

    use case

--- tm.sh
+++ tm.sh
@@ -5,24 +5,23 @@ if [ ! -e "$1" ]; then
 	exit 1
 fi
 
-BIN="$1"
-if echo "$1" | grep -E '^[^/]+$' >/dev/null; then
-	BIN=./"$1"
-fi
+case "$1" in
+	*/* ) BIN="$1";; 
+	*) BIN=./"$1" ;;
+esac
 
 DIR=$(dirname "$0")
 
 TYPE=$(file "$BIN")
 
-if echo "$TYPE" | grep 'ELF 64-bit' >/dev/null; then
-	:
-else
-	echo >&2 'FILE is not ELF 64-bit binary'
-	exit 1
-fi
+case "$TYPE" in
+	*ELF?64-bit* ) : ;;
+	*) printf >&2 '"%s" is not ELF 64-bit binary\n' "$BIN"
+	exit 1 ;;
+esac
 
-if echo "$TYPE" | grep 'dynamically linked' >/dev/null; then
-	echo >&2 'Hooking'
+case "$TYPE" in
+	*dynamically?linked* ) echo >&2 'Hooking'
 	if [ -z "$2" ]; then	
 		make >&2 -C "$DIR"
 	else
@@ -34,9 +33,8 @@ if echo "$TYPE" | grep 'dynamically linked' >/dev/null; then
 		echo
 		echo 'Execute This'
 		echo "env LD_PRELOAD=$DIR/hook.so $BIN"
-	fi
-elif echo "$TYPE" | grep 'statically linked' >/dev/null; then
-	echo >&2 'Patching'
+	fi ;;
+	*statically?linked* ) echo >&2 'Patching'
 	chmod +x "$DIR"/patcher.sh
 	if [ -z "$2" ]; then
 		"$DIR"/patcher.sh "$BIN"
@@ -50,9 +48,8 @@ elif echo "$TYPE" | grep 'statically linked' >/dev/null; then
 		echo
 		echo 'Execute This'
 		echo "${BIN}_patched"
-	fi
-else
-	echo >&2 'Unknown Type'
-	exit 1
-fi
+	fi ;;
+	*) echo >&2 'Unknown Type'
+	exit 1 ;;
+esac
 
